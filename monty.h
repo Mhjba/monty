094861ac723721d6a3e27029acf24831e1943a01
Mhjba <mahjoubaaitalla15@gmail.com>
2023-12-16 11:08:04 -0800

    monty

--- monty.h
+++ monty.h
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <stdbool.h>
 #include <ctype.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -14,7 +15,7 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
@@ -22,28 +23,17 @@ typedef struct stack_s
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
-
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-
-stack_t *add_dnodeint(stack_t **head, const int n);
-void push(stack_t **stack, unsigned int line, char *arg);
-void pall(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
-
-void free_stack(stack_t **head);
-void get_opcode_func(char *opcde, stack_t **stack, unsigned int line, FILE *f);
-void processfile(FILE *file);
-bool valid_arg(char *arg);
 #endif
