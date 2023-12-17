e8194d60dd92c005daba49004144ac86c620c850
Mhjba <mahjoubaaitalla15@gmail.com>
2023-12-17 10:47:16 -0800

    monty

--- push.c
+++ push.c
@@ -23,7 +23,7 @@ void _push(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	num = interpreter(opcode, line_number);
+	num = atoi(opcode);
 	element->n = num;
 	element->prev = NULL;
 	element->next = *stack;
