4c0915db192b65597bfb60ea2bed93b85a628b59
Mhjba <mahjoubaaitalla15@gmail.com>
2023-12-18 09:15:52 -0800

    monty

--- main.c
+++ main.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while ((read_line = getline(&content, &size, file)) != -1)
+	while ((read_line = getline(&content, &size, file)))
 	{
 		content = NULL;
 		bus.content = content;
