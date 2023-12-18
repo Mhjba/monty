dd62ff07c3411dd90ccc7a99218205164b0d2287
Mhjba <mahjoubaaitalla15@gmail.com>
2023-12-18 07:36:25 -0800

    monty

--- push.c
+++ push.c
@@ -8,6 +8,7 @@
 
 void push_func(stack_t **stack, unsigned int line_num)
 {
+	int n;
 	int i = 0, flag = 0;
 
 	if (bus.arg)
@@ -31,15 +32,12 @@ void push_func(stack_t **stack, unsigned int line_num)
 			exit(EXIT_FAILURE);
 		}
 	}
+
+	n = atoi(bus.arg);
+	if (bus.life_cycle == 0)
+		addnode(stack, n);
 	else
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_num);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*stack);
-		exit(EXIT_FAILURE);
-	}
-	/*n = atoi(bus.arg);*/
+		addqueue(stack, n);
 }
 
 /**
