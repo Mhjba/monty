348402b6ab16bd0e1639e71660abeec9585d2864
Mhjba <mahjoubaaitalla15@gmail.com>
2023-12-18 09:03:35 -0800

    monty

--- main.c
+++ main.c
@@ -22,7 +22,6 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
-	bus.file = file;
 	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
